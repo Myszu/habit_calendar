5e198071769fe0cd6774c1f7a77a1121260c2277
Marcin Nowacki <38333038+Myszu@users.noreply.github.com>
2023-09-14 10:16:47 +0200

    Update models.py
    Added table to hold tasks created by starting habit and renamed tables to fit the functionality.

--- modules/models.py
+++ modules/models.py
@@ -2,12 +2,14 @@ from sqlalchemy import Column, Integer, String, Date, Boolean
 from modules.database import Base
 
 
-class Tasks(Base):
-    __tablename__ = "tasks"
+class Habits(Base):
+    __tablename__ = "habits"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     user = Column(String)
+    habit_name = Column(String)
     start_date = Column(Date)
+    icon = Column(String)
     frequency = Column(Integer)
     quantity = Column(Integer)
    
@@ -19,3 +21,13 @@ class Users(Base):
     name = Column(String, unique=True)
     password = Column(String)
     admin = Column(Boolean)
+    
+
+class Tasks(Base):
+    __tablename__ = "tasks"
+    
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user = Column(String)
+    task_name = Column(String)
+    date = Column(Date)
+    state = Column(Boolean)
