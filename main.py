6e58879ffbea5950a939b054ddc5377b60f26893
Marcin Nowacki <38333038+Myszu@users.noreply.github.com>
2023-09-16 15:46:14 +0200

    Update 1.0.2
    - Added language choice in user panel

--- main.py
+++ main.py
@@ -49,16 +49,17 @@ class Main(ctk.CTk):
         # MENU FRAME
         self.menu_frame = ctk.CTkFrame(self, corner_radius=0, width=220)
         self.menu_frame.columnconfigure(0, weight=0)
+        self.menu_frame.rowconfigure(2, weight=1)
         self.menu_frame.grid(row=0, rowspan=2, column=0, padx=0, pady=0, sticky="nswe")
         
         self.current_user_label = ctk.CTkLabel(self.menu_frame, text=f'{self.lp_main[1]}', font=self.font_title)
         self.current_user_label.grid(row=0, column=0, padx=50, pady=(15, 5), sticky="we")
         self.username_label = ctk.CTkLabel(self.menu_frame, text=f'{self.active_user.name}', font=self.font_username)
         self.username_label.grid(row=1, column=0, padx=0, pady=(0, 10), sticky="we")
-        self.username_label.configure(text_color='#3a7ebf')
+        self.username_label.configure(text_color=self.main_color)
         
         self.user_panel_button = ctk.CTkButton(self.menu_frame, text=f'{self.lp_main[2]}', font=self.font_label, command=self.OpenUserPanel)
-        self.user_panel_button.grid(row=2, column=0, padx=40, pady=5, sticky="we")
+        self.user_panel_button.grid(row=2, column=0, padx=40, pady=(5, 15), sticky="swe")
         
         # CONTENT FRAME
         self.content_frame = ctk.CTkFrame(self)
@@ -99,7 +100,7 @@ class Main(ctk.CTk):
             card.columnconfigure(0, weight=1)
             
             if i < 7:
-                date_label = ctk.CTkLabel(card, text=f'{self.cal[i]}', font=self.font_weekend, text_color='#3a7ebf')
+                date_label = ctk.CTkLabel(card, text=f'{self.cal[i]}', font=self.font_weekend, text_color=self.main_color)
                 date_label.grid(row=0, column=0, padx=10, pady=0, sticky="w")
             else:
                 if int(self.cal[i].strftime("%w")) == 6 or int(self.cal[i].strftime("%w")) == 0:
@@ -118,8 +119,8 @@ class Main(ctk.CTk):
                 date_label.grid(row=0, column=0, padx=10, pady=(5, 0), sticky="nwe")
 
             if self.cal[i] == self.today:
-                card.configure(border_width=2, border_color='#3a7ebf')
-                date_label.configure(font=self.font_today, text_color='#3a7ebf')
+                card.configure(border_width=2, border_color=self.main_color)
+                date_label.configure(font=self.font_today, text_color=self.main_color)
                 
             self.cards.append(card)
             current_col += 1
@@ -130,17 +131,20 @@ class Main(ctk.CTk):
     def ApplyUserPref(self) -> None:
         """Applies users preferences to various settings.
         """
-        self.lp_main, self.lp_user_panel, self.lp_short_days, self.lp_full_days = self.language.LoadLangpack(self.active_user.language)
+        self.lp_main, self.lp_user_panel, self.languages_names, self.lp_short_days, self.lp_full_days = self.language.LoadLangpack(self.active_user.language)
         
         if self.active_user.theme == 'blue':
             ctk.set_default_color_theme("dark-blue")
+            self.main_color = '#3a7ebf'
         else:
             ctk.set_default_color_theme(self.active_user.theme)
+            self.main_color = self.active_user.theme
             
         if self.active_user.mode == 'default':
             ctk.set_appearance_mode('system')
         else:
             ctk.set_appearance_mode(self.active_user.mode)
+            
         
         
     def InitializeDB(self) -> None:
@@ -176,6 +180,7 @@ class Main(ctk.CTk):
         self.user_menu_frame = ctk.CTkFrame(self.user_panel, corner_radius=0, width=220)
         self.user_menu_frame.grid(row=0, column=0, padx=0, pady=0, sticky="nswe")
         
+        # USERNAME
         self.change_username_label = ctk.CTkLabel(self.user_menu_frame, text=f'{self.lp_user_panel[1]}', font=self.font_label)
         self.change_username_label.grid(row=0, column=0, columnspan=2, padx=15, pady=(10, 0), sticky="nswe")
         
@@ -186,12 +191,13 @@ class Main(ctk.CTk):
         self.change_username_button.grid(row=1, column=1, padx=(5, 20), pady=0, sticky="nswe")
         
         self.change_username_status = ctk.CTkLabel(self.user_menu_frame, text=f'', font=self.font_label)
-        self.change_username_status.grid(row=2, column=0, columnspan=2, padx=15, pady=(0, 10), sticky="nswe")
+        self.change_username_status.grid(row=2, column=0, columnspan=2, padx=15, pady=(0, 2), sticky="nswe")
         
-        self.change_password_label = ctk.CTkLabel(self.user_menu_frame, text=f'{self.lp_user_panel[10]}', font=self.font_label)
-        self.change_password_label.grid(row=3, column=0, columnspan=2, padx=15, pady=(10, 0), sticky="nswe")
+        # PASSWORD
+        self.change_password_label = ctk.CTkLabel(self.user_menu_frame, text=f'{self.lp_user_panel[2]}', font=self.font_label)
+        self.change_password_label.grid(row=3, column=0, columnspan=2, padx=15, pady=0, sticky="nswe")
         
-        self.change_password_entry = ctk.CTkEntry(self.user_menu_frame, placeholder_text="*********", show="*")
+        self.change_password_entry = ctk.CTkEntry(self.user_menu_frame, show="*")
         self.change_password_entry.grid(row=4, column=0, padx=(10, 5), pady=0, sticky="nswe")
         
         if self.active_user.password:
@@ -201,7 +207,17 @@ class Main(ctk.CTk):
         self.change_password_button.grid(row=4, column=1, padx=(5, 20), pady=0, sticky="nswe")
         
         self.change_password_status = ctk.CTkLabel(self.user_menu_frame, text=f'', font=self.font_label)
-        self.change_password_status.grid(row=5, column=0, columnspan=2, padx=15, pady=(0, 10), sticky="nswe")
+        self.change_password_status.grid(row=5, column=0, columnspan=2, padx=15, pady=(0, 5), sticky="nswe")
+        
+        # LANGUAGE
+        self.change_language_label = ctk.CTkLabel(self.user_menu_frame, text=f'{self.lp_user_panel[15]}', font=self.font_label)
+        self.change_language_label.grid(row=6, column=0, columnspan=2, padx=15, pady=0, sticky="nswe")
+        
+        self.change_language_combo = ctk.CTkComboBox(self.user_menu_frame, font=self.font_label, values=self.languages_names)
+        self.change_language_combo.grid(row=7, column=0, padx=15, pady=0, sticky="nswe")
+        
+        self.change_language_button = ctk.CTkButton(self.user_menu_frame, text=f'{self.lp_user_panel[4]}', width=40, font=self.font_label, command=self.ChangeLanguage)
+        self.change_language_button.grid(row=7, column=1, padx=(5, 20), pady=0, sticky="nswe")
         
         # CONTENT FRAME
         self.user_content_frame = ctk.CTkFrame(self.user_panel)
@@ -269,7 +285,25 @@ class Main(ctk.CTk):
         
         self.active_user = self.db.query(models.Users).where(models.Users.active == 1).one()
         self.change_password_status.configure(text=self.lp_user_panel[11], text_color="lightgreen")
+    
         
+    def ChangeLanguage(self) -> None:
+        match self.languages_names.index(self.change_language_combo.get()):
+            case 0:
+                chosen = 'pl'
+            case _:
+                chosen = 'en'
+        self.db.query(models.Users).where(models.Users.id == self.active_user.id).update({models.Users.language: chosen})
+        self.db.commit()
+        
+        self.active_user = self.db.query(models.Users).where(models.Users.active == 1).one()
+        self.ForceRefresh()
+        
+        
+    def ForceRefresh(self) -> None:
+        self.destroy()
+        self.__init__()
+    
     
     def OnClose(self):
         """Defines all actions to be done before closing the app.
