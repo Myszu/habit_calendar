dd1b538a3925da98df50865b4aaf74ebb273ad93
Marcin Nowacki <38333038+Myszu@users.noreply.github.com>
2023-09-24 21:58:32 +0200

    Update 1.0.13
    As usual - updated version :)

--- modules/settings.py
+++ modules/settings.py
@@ -10,7 +10,7 @@ FONT = cfg.get('Main', 'font')
 SHORTCUTS = cfg.getboolean('Main', 'shortcuts')
 THEME = cfg.get('Main', 'theme')
 MODE = cfg.get('Main', 'mode')
-VER = 'ver. 1.0.12'
+VER = 'ver. 1.0.13'
 
 def SaveChanges(section: str, setting: str, value) -> None:
     """Save changes made by user to the config file.
